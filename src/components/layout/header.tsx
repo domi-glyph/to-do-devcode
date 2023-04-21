f50d82b85fc9e41331fc124a7bd90deaaccceee9
Domi <domi.creativespace.gmail.com>
2023-04-21 14:36:20 +0700

    add header and image sources

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const HeaderLayout = () => {
+  return(
+    <Box backgroundColor="#16abf8">
+      <Text color="white" fontWeight={700} fontSize="9xl">TO DO LIST APP</Text>
+    </Box>
+  )
+}
+
+export default HeaderLayout;
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+const ActivityGroupPage = () =>{
+  
+}
+
+export default ActivityGroupPage;
